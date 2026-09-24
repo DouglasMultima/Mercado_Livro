50f5d810ce7bef9cb31861040903387000c2e74d
Douglas <douglas.multima@hotmail.com>
2026-09-23 20:59:35 -0400

    Mercado_livro_Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,6 +79,13 @@ dependencies {
 	implementation("org.flywaydb:flyway-mysql")
 	runtimeOnly("com.mysql:mysql-connector-j")
 
+
+	//SpringFox
+
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
+
+
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
